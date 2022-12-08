07971882efe978f0e3624ecc75bdd98e6c209f4c
Brianna Renni <briannaxrene@icloud.com>
2022-12-08 01:10:38 -0800

    Reset dark mode, localStorage not working

--- landing-pages/script.js
+++ landing-pages/script.js
@@ -2,30 +2,44 @@
 // ? loop theme toggle
 // ? persist theme
 
-// Theme switcher
 const themeBtn = document.querySelector('#theme-btn');
 let theme = document.documentElement.dataset.theme;
 
-const setDarkTheme = () => {
-    theme = 'dark';
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('data-theme', 'dark') // update local storage
-}
-const setLightTheme = () => {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('data-theme', 'light')
-}
+// document.documentElement.addEventListener('load', () => {
+//     var theme = localStorage.getItem('data_theme');
+//     switch (theme) {
+//         case 'light':
+//             setDarkTheme();
+//             break;
+//         case 'dark':
+//             setLightTheme();
+//             break;
+//     }
+// });
 
-themeBtn.addEventListener('click', () => {
-    switch (theme) {
-        case 'light':
-            setDarkTheme();
-            break;
-        case 'dark':
-            setLightTheme();
-            break;
-    }
-});
+// const setDarkTheme = (event) => {
+//     theme = 'dark';
+//     document.documentElement.setAttribute('data-theme', 'dark');
+//     localStorage.setItem('data-theme', 'dark') // update local storage
+//     event.preventDefault();
+// }
+// const setLightTheme = (event) => {
+//     document.documentElement.setAttribute('data-theme', 'light');
+//     localStorage.setItem('data-theme', 'light');
+//     event.preventDefault();
+// }
+
+// themeBtn.addEventListener('click', () => {
+//     switch (theme) {
+//         case 'light':
+//             setDarkTheme();
+//             break;
+//         case 'dark':
+//             setLightTheme();
+//             break;
+//     }
+//     themeBtn.dispatchEvent(new Event("click"))
+// });
 
 
 // TODO: Add page redirect to buttons
