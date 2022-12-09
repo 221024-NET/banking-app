fb8cb4f6116fa2e034f6b14e572a201976f259eb
Brianna Renni <briannaxrene@icloud.com>
2022-12-08 22:11:52 -0800

    Feature: site wide dark mode

--- landing-pages/theme.js
+++ landing-pages/theme.js
@@ -1,75 +1,30 @@
-// * Uses <body data-dark-theme> to toggle [data-dark-theme] CSS styles
-const body = document.body;
+// * Uses <body data-theme> to toggle [data-theme] CSS styles
 const themeBtn = document.querySelector('#theme-btn');
-const themeAttr = body.getAttribute('data-dark-theme');
-let modePref; // localStorage item
+let currentTheme;
 
-// On page load, check localStorage for theme preference
-window.onload = function () {
-    modePref = localStorage.getItem('modePref');
+const loadPref = () => {
+  let theme = localStorage.getItem('themePref');
+  if (theme !== null) {
+    currentTheme = theme;
+    setTheme(currentTheme);
+  } else {
+    currentTheme = 'light';
+    setTheme(currentTheme);
+  }
+};
 
-}
+const setTheme = (theme) => {
+  let pressed = theme === 'dark' ? 'true' : 'false';
+  themeBtn.setAttribute('aria-pressed', pressed);
+  document.documentElement.setAttribute('data-theme', theme);
+  localStorage.setItem('themePref', theme);
+};
 
-// * Event Handler
-const toggleTheme = () => {
-    let isDark = body.classList.contains('data-dark-theme');
-    // * if body has darkTheme, remove it and set modepref to 'light'
-    // * if not, add it and set modepref to 'dark'
-}
+let toggleTheme = () => {
+  document.documentElement.getAttribute('data-theme') === 'dark'
+    ? setTheme('light')
+    : setTheme('dark');
+};
 
-
-
-// toggleTheme()
-// Event listener for dark mode toggle
-
-
-
-// ! Method 2 (using class)
-// * Check if dark mode is enabled
-const darkMode = localStorage.getItem('darkMode');
-
-// * If dark mode is enabled, apply the dark mode styles to the page
-if (darkMode === 'true') {
-    document.body.classList.add('dark-mode');
-}
-
-// * When the toggle button is clicked, toggle the dark mode state and update page styles
-toggleButton.addEventListener('click', () => {
-    if (darkMode === 'true') {
-        localStorage.setItem('darkMode', 'false');
-        document.body.classList.remove('dark-mode');
-    } else {
-        localStorage.setItem('darkMode', 'true');
-        document.body.classList.add('dark-mode');
-    }
-});
-
-
-// ! Method 3 (Toggle works, no persist)
-
-// const setDarkTheme = (event) => {
-//     theme = 'dark';
-//     document.documentElement.setAttribute('data-theme', 'dark');
-//     localStorage.setItem('data-theme', 'dark') // update local storage
-//     event.preventDefault();
-// }
-// const setLightTheme = (event) => {
-//     document.documentElement.setAttribute('data-theme', 'light');
-//     localStorage.setItem('data-theme', 'light');
-//     event.preventDefault();
-// }
-
-// themeBtn.addEventListener('click', () => {
-//     switch (theme) {
-//         case 'light':
-//             setDarkTheme();
-//             break;
-//         case 'dark':
-//             setLightTheme();
-//             break;
-//     }
-//     themeBtn.dispatchEvent(new Event("click"))
-// });
-
-
-// TODO: Add page redirect to buttons
+themeBtn.addEventListener('click', toggleTheme);
+document.addEventListener('DOMContentLoaded', loadPref);
