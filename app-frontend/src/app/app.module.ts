c3e44e0134aa60b9eedcf8f6810063c9833a72e6
Brianna Renni <briannaxrene@icloud.com>
2022-12-12 17:55:40 -0800

    Create base folders in src/app, generate components for page layout

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
+import { HeaderComponent } from './shared-mods/header/header.component';
+import { ThemeBtnComponent } from './shared-mods/theme-btn/theme-btn.component';
+import { LoginBtnComponent } from './landing-pages/components/login-btn/login-btn.component';
+import { SignupBtnComponent } from './landing-pages/components/signup-btn/signup-btn.component';
+import { LoginFormComponent } from './landing-pages/components/login-form/login-form.component';
+import { SignupFormComponent } from './landing-pages/components/signup-form/signup-form.component';
+import { ForgotFormComponent } from './landing-pages/components/forgot-form/forgot-form.component';
+import { DashboardComponent } from './user-pages/dashboard/dashboard.component';
+import { ProfileComponent } from './user-pages/profile/profile.component';
+import { ProfileBtnComponent } from './user-pages/components/profile-btn/profile-btn.component';
+import { LogoutBtnComponent } from './user-pages/components/logout-btn/logout-btn.component';
+import { LoginComponent } from './landing-pages/login/login.component';
+import { SignupComponent } from './landing-pages/signup/signup.component';
+import { ForgotPwComponent } from './landing-pages/forgot-pw/forgot-pw.component';
+import { UserNavComponent } from './user-pages/components/user-nav/user-nav.component';
+import { LandingNavComponent } from './landing-pages/components/landing-nav/landing-nav.component';
+import { FooterComponent } from './shared-mods/footer/footer.component';
+import { MainComponent } from './shared-mods/main/main.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ThemeBtnComponent,
+    LoginBtnComponent,
+    SignupBtnComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    ForgotFormComponent,
+    DashboardComponent,
+    ProfileComponent,
+    ProfileBtnComponent,
+    LogoutBtnComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotPwComponent,
+    UserNavComponent,
+    LandingNavComponent,
+    FooterComponent,
+    MainComponent,
+    ForgotFormComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
