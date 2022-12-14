dc7e056c45c67d9b6eb8357d8fb99131f31ec07f
Brianna Renni <briannaxrene@icloud.com>
2022-12-14 00:12:17 -0800

    CSS build running clean

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -5,19 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './Users/briannarene/_code/net-c-projs/p2-remake/app-frontend/src/main/main.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [			
-    AppComponent,
-      FooterComponent,
-      HeaderComponent,
-      MainComponent
-   ],
-  imports: [
-    BrowserModule, HttpClientModule
-  ],
+  declarations: [AppComponent, FooterComponent, HeaderComponent, MainComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
