7e0f5bada831ffcfa895831f5a04d0bd34482a61
pisithsork <55104592+pisithsork@users.noreply.github.com>
2022-12-18 18:43:04 -0500

    Merged with Account-Angular-API physically

---
+++ app-frontend/src/app/classes/accountobject.ts
@@ -0,0 +1,22 @@
+import { IAccount } from './accountinterface';
+
+export class Account implements IAccount {
+    acct_Id: number;
+    user_Id: number;
+    type: string;
+    balance?: number;
+
+//   constructor(acct_Id: number, user_Id: number,type: string, balance?: number) {
+//     this.acct_Id = acct_Id;
+//     this.user_Id = user_Id;
+//     this.type = type;
+//     this.balance = balance;
+//   }
+    constructor(account: IAccount){
+        this.acct_Id = account.acct_Id;
+        this.user_Id = account.user_Id;
+        this.type = account.type;
+        this.balance = account.balance;
+    }
+
+}
