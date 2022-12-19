5bbe0385c5fa1a1f6feeb5d118fb849ac3fa8f2f
Brianna Renni <briannaxrene@icloud.com>
2022-12-18 17:33:42 -0800

    Delete

--- Tests/Usings.cs
+++
@@ -1,8 +0,0 @@
-global using Xunit;
-global using app_backend.Controllers;
-global using app_backend.Models;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.EntityFrameworkCore;
-global using System.Net.Mime;
-global using Xunit.Abstractions;
-global using Xunit.Sdk;
