782a8202ae259b2dae6696d77a4cc66e37f1938d
Brianna Renni <briannaxrene@icloud.com>
2022-12-18 16:03:59 -0800

    Delete landing

--- app-backend/Models/User.cs
+++ app-backend/Models/User.cs
@@ -6,10 +6,10 @@ namespace app_backend.Models
     {
         [Key]
         public int user_ID { get; set; }
-        public string email { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public string address { get; set; }
-        public string password { get; set; }
+        public string? email { get; set; }
+        public string? first_name { get; set; }
+        public string? last_name { get; set; }
+        public string? address { get; set; }
+        public string? password { get; set; }
     }
 }
