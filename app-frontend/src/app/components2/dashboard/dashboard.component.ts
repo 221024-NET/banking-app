f57740c9c6e4ec2e9a7e8dbb004dfb88f1b1746d
KevinCornell <kevdcornell@gmail.com>
2022-12-18 19:30:02 -0500

    Modified dashboard.

--- app-frontend/src/app/components2/dashboard/dashboard.component.ts
+++ app-frontend/src/app/components2/dashboard/dashboard.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Account } from './../../classes/accountobject';
+import { CurrentuserService } from '../../services/currentuser.service';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'app/classes/userobject';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
-  constructor(private router: Router) {}
+export class DashboardComponent implements OnInit {
+  currentUser: User = new User(0, "", "", "", "", "");
+  currentAccnts: Account[] = [];
+
+  constructor(private router: Router, private userSevice: CurrentuserService) {}
+
+  ngOnInit(): void {
+    this.currentUser = this.userSevice.getData();
+    this.currentAccnts = this.userSevice.getAccts();
+  }
 
   goProfile() {
     this.router.navigate(['/profile']);
