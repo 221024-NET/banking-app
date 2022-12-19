d45db94daeb89b87cfbf49e8459aba33725cd8aa
KevinCornell <kevdcornell@gmail.com>
2022-12-18 22:31:42 -0500

    Transaction form User is able to pass in input to make a transaction. This transaction is NOT being sent to the db as of this commit.

--- app-frontend/src/app/components2/send/send.component.ts
+++ app-frontend/src/app/components2/send/send.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Transaction } from 'app/classes/transactionobject';
+import { TransactionForm } from 'app/classes/transactionformobject';
+import { CurrentuserService } from 'app/services/currentuser.service';
+import { NavbarService } from 'app/services/navbar.service';
+import { LoginService } from '../../services/superservice.service';
 
 @Component({
   selector: 'app-send',
@@ -8,10 +14,34 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class SendComponent implements OnInit {
   formdata: any;
+  formTrans = new TransactionForm(0, 0, 0, "", 0);
 
-  constructor() { }
+  constructor(
+    private service: LoginService,
+    private Currentuser: CurrentuserService,
+    private router: Router,
+    private navbarService: NavbarService
+  ) { }
 
   ngOnInit() {
+    this.formdata = new FormGroup({
+      src_acct: new FormControl(''),
+      dst_acct: new FormControl(''),
+      amount: new FormControl('')
+    });
+  }
+
+  PostToTransaction(transaction: Transaction) {
+    if (!this.formdata.valid) {
+      this.formdata.markAllAsTouched();
+    } else {
+      transaction.ref_Id = 0;
+      transaction.status = "approved";
+    }
+  }
+
+  goBack() {
+    this.router.navigateByUrl('/dashboard');
   }
 
 }
